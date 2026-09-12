dace48d05b7c376c73790cd446c171558fef76e3
Wes Dean <wesley-dean@users.noreply.github.com>
2026-09-11 20:03:55 -0400

    test: add method golden output

---
+++ tests/python/expected/07-method-docstring.py
@@ -0,0 +1,10 @@
+class Repository:
+    """Represent a repository."""
+
+    def load(self, path: str) -> str:
+        """Load a repository path.
+
+        @param path Repository path to load.
+        @return The loaded path.
+        """
+        return path
