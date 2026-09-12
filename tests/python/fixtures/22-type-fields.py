875331922692a124d50ded84595dc3c4869c7302
Wes Dean <wesley-dean@users.noreply.github.com>
2026-09-11 22:43:31 -0400

    test: add type field fixture

---
+++ tests/python/fixtures/22-type-fields.py
@@ -0,0 +1,9 @@
+def load_unannotated(path):
+    """Load one unannotated value.
+
+    :param path: Path to read.
+    :type path: pathlib.Path
+    :returns: Loaded configuration.
+    :rtype: Configuration
+    """
+    return path
